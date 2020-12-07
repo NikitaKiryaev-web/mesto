0e82fe36f7a70309b155b628163229bab79db890
Nikita Kiryaev <kiryaev.work@yandex.ru>
2020-12-08 02:00:40 +0300

    fix: исправить замечания ревью

--- scripts/Constants.js
+++ scripts/Constants.js
@@ -1,3 +1,11 @@
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error'
+};
+
 export const initialCards = [
   {
       name: 'Архыз',
